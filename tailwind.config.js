9fce1bf2cc8478e6d50b7867d545bb7b2d8be081
jocnn <ijann_@outlook.com>
2022-05-20 15:10:13 -0300

    para detectar el tailwind en otras carpetas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./layout/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
