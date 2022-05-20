125e01be4bbbc62391cb30252ebbdb40c1231fc8
jocnn <ijann_@outlook.com>
2022-05-20 14:53:18 -0300

    consultando la db con getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client"
+
+export default function Home({ categorias }) {
+
+  console.info(categorias)
+  
   return (
     <div>
       <h1>NextJS</h1>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+
+  const categorias = await prisma.categoria.findMany()
+  // console.info(categorias)
+  
+  return {
+    props: {
+      categorias,
+    }
+  }
+  
 }
