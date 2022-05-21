5c4aefe202dad58edcd51b03cd869b4b40f4c1c2
jocnn <ijann_@outlook.com>
2022-05-21 08:21:44 -0300

    consumiendo la api desde el provider

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,13 +1,29 @@
-import { createContext } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
 const QuioscoContext = createContext()
 
 const QuioscoProvider = ({children}) => {
 
+  const [ categorias, setCategorias ] = useState([])
+
+  useEffect(() => {
+    
+    obtenerCategorias()
+
+  }, [])
+
+  const obtenerCategorias = async () => {
+    const { data } = await axios('/api/categorias')
+
+    setCategorias(data)
+
+  }
+
   return (
     <QuioscoContext.Provider 
       value={{
-        
+        categorias,
       }}
     >
       {children}
