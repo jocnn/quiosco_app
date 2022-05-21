abfbd86a09f029a8ee94fc83189cb1a963626d23
jocnn <ijann_@outlook.com>
2022-05-21 08:22:55 -0300

    mostrando las categorias desde el provider

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,12 @@
 import Image from "next/image"
+import useQuiosco from "../hooks/useQuiosco"
+
+import Categoria from "./Categoria"
 
 const Sidebar = () => {
+  
+  const { categorias } = useQuiosco()
+  
   return (
     <>
       <Image
@@ -9,6 +15,17 @@ const Sidebar = () => {
         src="/assets/img/logo.svg"
         alt="imagen logotipo"
       />
+
+      <nav className="mt-10">
+        {
+          categorias.map( categoria => (
+            <Categoria 
+              key={categoria.id}
+              categoria={categoria}
+            />
+          ))
+        }
+      </nav>
     </>
   )
 }
