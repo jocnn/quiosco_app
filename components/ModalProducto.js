e188e35880f1afdfa9ad5401791934092935aa64
jocnn <ijann_@outlook.com>
2022-05-21 17:54:35 -0300

    cerrando modal al realizar pedido

--- components/ModalProducto.js
+++ components/ModalProducto.js
@@ -93,7 +93,10 @@ const ModalProducto = () => {
         <button
           type="button"
           className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
-          onClick={ () => handleAgregarPedido({...producto, cantidad}) }
+          onClick={ () => {
+            handleChangeModal()
+            handleAgregarPedido({...producto, cantidad})
+          }}
         >
           Añadir al Pedido
         </button>
