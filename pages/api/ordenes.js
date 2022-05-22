00032b516e67e48167b15518ab521ccad63a979d
jocnn <ijann_@outlook.com>
2022-05-22 11:14:13 -0300

    insertando registro en la db

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -1,6 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+
 export default async function handler(req, res) {
+  const prisma = new PrismaClient()
+
   if (req.method === 'POST') {
-    console.log(req.body)
-    res.json({ method: "POST!!!" })
+    const orden = await prisma.orden.create({
+      data: {
+        nombre: req.body.nombre,
+        total: req.body.total,
+        pedidos: req.body.pedido,
+        fecha: req.body.fecha,
+      }
+    })
+    res.json(orden)
   }
 }
